738465a07889292328453edfc8deb20bb45a4a72
Jon Halliday <jhalliday@gmail.com>
2018-01-25 09:52:10 -0500

    Better performing Transparent Overlay Window

--- Assets/Scripts/Interop/NativeMethods.cs
+++ Assets/Scripts/Interop/NativeMethods.cs
@@ -5,236 +5,44 @@ namespace Assets.Scripts.Interop
 {
     public static class NativeMethods
     {
-        public struct Point
-        {
-            public int x;
-            public int y;
-
-            public Point(int x, int y)
-            {
-                this.x = x;
-                this.y = y;
-            }
-        }
-
-        public struct Size
-        {
-            public int cx;
-            public int cy;
-
-            public Size(int cx, int cy)
-            {
-                this.cx = cx;
-                this.cy = cy;
-            }
-        }
-
-        public struct BlendFunction
-        {
-            public byte BlendOp;
-            public byte BlendFlags;
-            public byte SourceConstantAlpha;
-            public byte AlphaFormat;
-        }
-
-        /// <summary>
-        /// Window handles (HWND) used for hWndInsertAfter
-        /// </summary>
-        public static class Hwnd
-        {
-            public static IntPtr
-                NoTopMost = new IntPtr(-2),
-                TopMost = new IntPtr(-1),
-                Top = new IntPtr(0),
-                Bottom = new IntPtr(1);
-        }
-
-        [Flags]
-        public enum SetWindowPosFlags : uint
-        {
-            /// <summary>If the calling thread and the thread that owns the window are attached to different input queues, 
-            /// the system posts the request to the thread that owns the window. This prevents the calling thread from 
-            /// blocking its execution while other threads process the request.</summary>
-            /// <remarks>SWP_ASYNCWINDOWPOS</remarks>
-            AsynchronousWindowPosition = 0x4000,
-            /// <summary>Prevents generation of the WM_SYNCPAINT message.</summary>
-            /// <remarks>SWP_DEFERERASE</remarks>
-            DeferErase = 0x2000,
-            /// <summary>Draws a frame (defined in the window's class description) around the window.</summary>
-            /// <remarks>SWP_DRAWFRAME</remarks>
-            DrawFrame = 0x0020,
-            /// <summary>Applies new frame styles set using the SetWindowLong function. Sends a WM_NCCALCSIZE message to 
-            /// the window, even if the window's size is not being changed. If this flag is not specified, WM_NCCALCSIZE 
-            /// is sent only when the window's size is being changed.</summary>
-            /// <remarks>SWP_FRAMECHANGED</remarks>
-            FrameChanged = 0x0020,
-            /// <summary>Hides the window.</summary>
-            /// <remarks>SWP_HIDEWINDOW</remarks>
-            HideWindow = 0x0080,
-            /// <summary>Does not activate the window. If this flag is not set, the window is activated and moved to the 
-            /// top of either the topmost or non-topmost group (depending on the setting of the hWndInsertAfter 
-            /// parameter).</summary>
-            /// <remarks>SWP_NOACTIVATE</remarks>
-            DoNotActivate = 0x0010,
-            /// <summary>Discards the entire contents of the client area. If this flag is not specified, the valid 
-            /// contents of the client area are saved and copied back into the client area after the window is sized or 
-            /// repositioned.</summary>
-            /// <remarks>SWP_NOCOPYBITS</remarks>
-            DoNotCopyBits = 0x0100,
-            /// <summary>Retains the current position (ignores X and Y parameters).</summary>
-            /// <remarks>SWP_NOMOVE</remarks>
-            IgnoreMove = 0x0002,
-            /// <summary>Does not change the owner window's position in the Z order.</summary>
-            /// <remarks>SWP_NOOWNERZORDER</remarks>
-            DoNotChangeOwnerZOrder = 0x0200,
-            /// <summary>Does not redraw changes. If this flag is set, no repainting of any kind occurs. This applies to 
-            /// the client area, the nonclient area (including the title bar and scroll bars), and any part of the parent 
-            /// window uncovered as a result of the window being moved. When this flag is set, the application must 
-            /// explicitly invalidate or redraw any parts of the window and parent window that need redrawing.</summary>
-            /// <remarks>SWP_NOREDRAW</remarks>
-            DoNotRedraw = 0x0008,
-            /// <summary>Same as the SWP_NOOWNERZORDER flag.</summary>
-            /// <remarks>SWP_NOREPOSITION</remarks>
-            DoNotReposition = 0x0200,
-            /// <summary>Prevents the window from receiving the WM_WINDOWPOSCHANGING message.</summary>
-            /// <remarks>SWP_NOSENDCHANGING</remarks>
-            DoNotSendChangingEvent = 0x0400,
-            /// <summary>Retains the current size (ignores the cx and cy parameters).</summary>
-            /// <remarks>SWP_NOSIZE</remarks>
-            IgnoreResize = 0x0001,
-            /// <summary>Retains the current Z order (ignores the hWndInsertAfter parameter).</summary>
-            /// <remarks>SWP_NOZORDER</remarks>
-            IgnoreZOrder = 0x0004,
-            /// <summary>Displays the window.</summary>
-            /// <remarks>SWP_SHOWWINDOW</remarks>
-            ShowWindow = 0x0040,
-        }
-
-        /// <summary>
-        ///     Specifies a raster-operation code. These codes define how the color data for the
-        ///     source rectangle is to be combined with the color data for the destination
-        ///     rectangle to achieve the final color.
-        /// </summary>
-        public enum TernaryRasterOperations : uint
-        {
-            /// <summary>dest = source</summary>
-            SRCCOPY = 0x00CC0020,
-            /// <summary>dest = source OR dest</summary>
-            SRCPAINT = 0x00EE0086,
-            /// <summary>dest = source AND dest</summary>
-            SRCAND = 0x008800C6,
-            /// <summary>dest = source XOR dest</summary>
-            SRCINVERT = 0x00660046,
-            /// <summary>dest = source AND (NOT dest)</summary>
-            SRCERASE = 0x00440328,
-            /// <summary>dest = (NOT source)</summary>
-            NOTSRCCOPY = 0x00330008,
-            /// <summary>dest = (NOT src) AND (NOT dest)</summary>
-            NOTSRCERASE = 0x001100A6,
-            /// <summary>dest = (source AND pattern)</summary>
-            MERGECOPY = 0x00C000CA,
-            /// <summary>dest = (NOT source) OR dest</summary>
-            MERGEPAINT = 0x00BB0226,
-            /// <summary>dest = pattern</summary>
-            PATCOPY = 0x00F00021,
-            /// <summary>dest = DPSnoo</summary>
-            PATPAINT = 0x00FB0A09,
-            /// <summary>dest = pattern XOR dest</summary>
-            PATINVERT = 0x005A0049,
-            /// <summary>dest = (NOT dest)</summary>
-            DSTINVERT = 0x00550009,
-            /// <summary>dest = BLACK</summary>
-            BLACKNESS = 0x00000042,
-            /// <summary>dest = WHITE</summary>
-            WHITENESS = 0x00FF0062,
-            /// <summary>
-            /// Capture window as seen on screen.  This includes layered windows 
-            /// such as WPF windows with AllowsTransparency="true"
-            /// </summary>
-            CAPTUREBLT = 0x40000000
-        }
-
-        [DllImport("user32.dll")]
-        public static extern IntPtr GetWindowLong(IntPtr hWnd, int nIndex);
-
-        // This helper static method is required because the 32-bit version of user32.dll does not contain this API
-        // (on any versions of Windows), so linking the method will fail at run-time. The bridge dispatches the request
-        // to the correct function (GetWindowLong in 32-bit mode and GetWindowLongPtr in 64-bit mode)
-        public static IntPtr SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong)
-        {
-            if (IntPtr.Size == 8)
-            {
-                return SetWindowLongPtr64(hWnd, nIndex, dwNewLong);
-            }
-
-            return new IntPtr(SetWindowLong32(hWnd, nIndex, dwNewLong.ToInt32()));
-        }
-
-        [DllImport("user32.dll", EntryPoint = "SetWindowLong")]
-        private static extern int SetWindowLong32(IntPtr hWnd, int nIndex, int dwNewLong);
-
-        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr")]
-        private static extern IntPtr SetWindowLongPtr64(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
-
         [DllImport("user32.dll")]
         public static extern IntPtr GetActiveWindow();
 
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, SetWindowPosFlags uFlags);
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
 
-        [DllImport("User32.dll")]
-        public static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
 
-        [DllImportAttribute("user32.dll")]
-        public static extern int UpdateLayeredWindow(IntPtr handle, IntPtr hdcDst, ref Point pptDst, ref Size psize, IntPtr hdcSrc, ref Point pprSrc, int crKey, ref BlendFunction pblend, int dwFlags);
+        [DllImport("user32.dll", EntryPoint = "SetLayeredWindowAttributes")]
+        public static extern int SetLayeredWindowAttributes(IntPtr hwnd, int crKey, byte bAlpha, int dwFlags);
 
-        [DllImportAttribute("user32.dll")]
-        public static extern IntPtr GetDC(IntPtr handle);
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        public static extern int SetWindowPos(IntPtr hwnd, int hwndInsertAfter, int x, int y, int cx, int cy, int uFlags);
 
-        [DllImportAttribute("user32.dll", ExactSpelling = true)]
-        public static extern int ReleaseDC(IntPtr handle, IntPtr hDC);
+        [DllImport("Dwmapi.dll")]
+        public static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref WinApi.MARGINS margins);
+    }
 
-        [DllImportAttribute("gdi32.dll")]
-        public static extern IntPtr CreateCompatibleDC(IntPtr hDC);
+    public static class WinApi
+    {
+        public const int GWL_STYLE = -16;
+        public const int GWL_EXSTYLE = -20;
+        public const int WS_EX_LAYERED = 524288;
+        public const int WS_EX_TRANSPARENT = 32;
+        public const uint WS_POPUP = 0x80000000;
+        public const uint WS_VISIBLE = 0x10000000;
+        public const int HWND_TOPMOST = -1;
+        public const int LWA_ALPHA = 2;
+        public const int SWP_FRAMECHANGED = 32;
+        public const int SWP_SHOWWINDOW = 64;
 
-        /// <summary>
-        ///        Creates a bitmap compatible with the device that is associated with the specified device context.
-        /// </summary>
-        /// <param name="hdc">A handle to a device context.</param>
-        /// <param name="nWidth">The bitmap width, in pixels.</param>
-        /// <param name="nHeight">The bitmap height, in pixels.</param>
-        /// <returns>If the function succeeds, the return value is a handle to the compatible bitmap (DDB). If the function fails, the return value is <see cref="System.IntPtr.Zero"/>.</returns>
-        [DllImport("gdi32.dll", EntryPoint = "CreateCompatibleBitmap")]
-        public static extern IntPtr CreateCompatibleBitmap([In] IntPtr hdc, int nWidth, int nHeight);
-
-        /// <summary>
-        ///    Performs a bit-block transfer of the color data corresponding to a
-        ///    rectangle of pixels from the specified source device context into
-        ///    a destination device context.
-        /// </summary>
-        /// <param name="hdc">Handle to the destination device context.</param>
-        /// <param name="nXDest">The leftmost x-coordinate of the destination rectangle (in pixels).</param>
-        /// <param name="nYDest">The topmost y-coordinate of the destination rectangle (in pixels).</param>
-        /// <param name="nWidth">The width of the source and destination rectangles (in pixels).</param>
-        /// <param name="nHeight">The height of the source and the destination rectangles (in pixels).</param>
-        /// <param name="hdcSrc">Handle to the source device context.</param>
-        /// <param name="nXSrc">The leftmost x-coordinate of the source rectangle (in pixels).</param>
-        /// <param name="nYSrc">The topmost y-coordinate of the source rectangle (in pixels).</param>
-        /// <param name="dwRop">A raster-operation code.</param>
-        /// <returns>
-        ///    <c>true</c> if the operation succeedes, <c>false</c> otherwise. To get extended error information, call <see cref="System.Runtime.InteropServices.Marshal.GetLastWin32Error"/>.
-        /// </returns>
-        [DllImport("gdi32.dll", EntryPoint = "BitBlt", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool BitBlt([In] IntPtr hdc, int nXDest, int nYDest, int nWidth, int nHeight, [In] IntPtr hdcSrc, int nXSrc, int nYSrc, TernaryRasterOperations dwRop);
-
-        [DllImportAttribute("gdi32.dll")]
-        public static extern int DeleteDC(IntPtr hdc);
-
-        [DllImportAttribute("gdi32.dll")]
-        public static extern IntPtr SelectObject(IntPtr hDC, IntPtr hObject);
-
-        [DllImportAttribute("gdi32.dll")]
-        public static extern int DeleteObject(IntPtr hObject);
+        public struct MARGINS
+        {
+            public int cxLeftWidth;
+            public int cxRightWidth;
+            public int cyTopHeight;
+            public int cyBottomHeight;
+        }
     }
 }
--- Assets/Scripts/TransparentWindow.cs
+++ Assets/Scripts/TransparentWindow.cs
@@ -1,86 +1,32 @@
-﻿using System;
-using System.Runtime.InteropServices;
+﻿using Assets.Scripts.Interop;
 using UnityEngine;
 
 namespace Assets.Scripts
 {
     public class TransparentWindow : MonoBehaviour
     {
-        private struct MARGINS
-        {
-            public int cxLeftWidth;
-            public int cxRightWidth;
-            public int cyTopHeight;
-            public int cyBottomHeight;
-        }
-
-        // Define function signatures to import from Windows APIs
-        [DllImport("user32.dll")]
-        private static extern IntPtr GetActiveWindow();
-
-        [DllImport("user32.dll")]
-        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
-
-        [DllImport("Dwmapi.dll")]
-        private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
-
-        // Definitions of window styles
-        private const int GWL_STYLE = -16;
-        private const uint WS_POPUP = 0x80000000;
-        private const uint WS_VISIBLE = 0x10000000;
-
-        private bool _isOverlayActive;
-
         public Material Material;
 
         public void Start()
         {
-//#if !UNITY_EDITOR
-//        var margins = new MARGINS() { cxLeftWidth = -1 };
- 
-//        // Get a handle to the window
-//        var hwnd = GetActiveWindow();
- 
-//        // Set properties of the window
-//        // See: https://msdn.microsoft.com/en-us/library/windows/desktop/ms633591%28v=vs.85%29.aspx
-//        SetWindowLong(hwnd, GWL_STYLE, WS_POPUP | WS_VISIBLE);
-         
-//        // Extend the window into the client area
-//        // See: https://msdn.microsoft.com/en-us/library/windows/desktop/aa969512%28v=vs.85%29.aspx 
-//        DwmExtendFrameIntoClientArea(hwnd, ref margins);
-//#endif
+            #if !UNITY_EDITOR // You really don't want to enable this in the editor..
+
+            int fWidth = Screen.width;
+            int fHeight = Screen.height;
+            var margins = new WinApi.MARGINS { cxLeftWidth = -1 };
+            var hwnd = NativeMethods.GetActiveWindow();
+
+            //NativeMethods.SetWindowLong(hwnd, WinApi.GWL_STYLE, WinApi.WS_POPUP | WinApi.WS_VISIBLE);
+
+            // Transparent windows with click through
+            NativeMethods.SetWindowLong(hwnd, WinApi.GWL_EXSTYLE, WinApi.WS_EX_LAYERED | WinApi.WS_EX_TRANSPARENT);
+            NativeMethods.SetLayeredWindowAttributes(hwnd, 0, 255, WinApi.LWA_ALPHA);
+            NativeMethods.SetWindowPos(hwnd, WinApi.HWND_TOPMOST, 0, 0, fWidth, fHeight, WinApi.SWP_FRAMECHANGED | WinApi.SWP_SHOWWINDOW);
+            NativeMethods.DwmExtendFrameIntoClientArea(hwnd, ref margins);
+
+            #endif
         }
 
-        public void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                if (!_isOverlayActive)
-                {
-                    var margins = new MARGINS() { cxLeftWidth = -1 };
-
-                    // Get a handle to the window
-                    var hwnd = GetActiveWindow();
-
-                    // Set properties of the window
-                    // See: https://msdn.microsoft.com/en-us/library/windows/desktop/ms633591%28v=vs.85%29.aspx
-                    SetWindowLong(hwnd, GWL_STYLE, WS_POPUP | WS_VISIBLE);
-
-                    // Extend the window into the client area
-                    // See: https://msdn.microsoft.com/en-us/library/windows/desktop/aa969512%28v=vs.85%29.aspx 
-                    DwmExtendFrameIntoClientArea(hwnd, ref margins);
-                }
-                else
-                {
-                    
-                }
-
-                _isOverlayActive = !_isOverlayActive;
-            }
-        }
-
-        // Pass the output of the camera to the custom material
-        // for chroma replacement
         public void OnRenderImage(RenderTexture from, RenderTexture to)
         {
             Graphics.Blit(from, to, Material);

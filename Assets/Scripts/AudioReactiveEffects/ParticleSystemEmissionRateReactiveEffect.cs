286c5c2708af193d331cd57ed7b45ed47a4ed022
Jon Halliday <Jon.Halliday@insurity.com>
2018-01-26 15:47:50 -0500

    Added particle system and associated scripts

---
+++ Assets/Scripts/AudioReactiveEffects/ParticleSystemEmissionRateReactiveEffect.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts.AudioReactiveEffects.Base;
+using UnityEngine;
+
+namespace Assets.Scripts.AudioReactiveEffects
+{
+    public class ParticleSystemEmissionRateReactiveEffect : VisualizationEffectBase
+    {
+        private ParticleSystem _particleSystem;
+        private ParticleSystem.EmissionModule _emissionModule;
+        private ParticleSystem.MinMaxCurve _emissionRate;
+
+        public override void Start()
+        {
+            base.Start();
+
+            _particleSystem = GetComponent<ParticleSystem>();
+            _emissionModule = _particleSystem.emission;
+            _emissionRate = _particleSystem.emission.rateOverTime;
+        }
+
+        public void Update()
+        {
+            int rate = (int)(GetAudioData() * PrimaryScaleFactor);
+            _emissionRate.constant = rate;
+            _emissionRate.constantMax = rate;
+            _emissionRate.constantMin = rate;
+            _emissionModule.rateOverTime = _emissionRate;
+        }
+    }
+}
---
+++ Assets/Scripts/AudioReactiveEffects/ParticleSystemStartSpeedReactiveEffect.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.AudioReactiveEffects.Base;
+using UnityEngine;
+
+namespace Assets.Scripts.AudioReactiveEffects
+{
+    public class ParticleSystemStartSpeedReactiveEffect : VisualizationEffectBase
+    {
+        private ParticleSystem _particleSystem;
+        private ParticleSystem.MainModule _mainModule;
+
+        public override void Start()
+        {
+            base.Start();
+
+            _particleSystem = GetComponent<ParticleSystem>();
+            _mainModule = _particleSystem.main;
+        }
+
+        public void Update()
+        {
+            _mainModule.startSpeed = GetAudioData() * PrimaryScaleFactor;
+        }
+    }
+}

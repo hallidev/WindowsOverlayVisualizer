51056006d89ee8ea6f706755c14e076dbebf12aa
Jon Halliday <jhalliday@gmail.com>
2018-01-27 14:38:46 -0500

    Direction facing

---
+++ Assets/Scripts/Rigidbody/RigidbodiesFaceDirection.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Rigidbody
+{
+    public class RigidbodiesFaceDirection : MonoBehaviour
+    {
+        private RigidbodyFaceDirection[] _rigidbodyFaceDirections;
+
+        public Vector3 FacingDirection = Vector3.zero;
+
+        public void Start()
+        {
+            _rigidbodyFaceDirections = GetComponentsInChildren<RigidbodyFaceDirection>();
+        }
+
+        public void FixedUpdate()
+        {
+            for (int i = 0; i < _rigidbodyFaceDirections.Length; i++)
+            {
+                _rigidbodyFaceDirections[i].FacingDirection = FacingDirection;
+            }
+        }
+    }
+}

fbfa3dc339ab244f045ce9d7c5740e1cd0f7868b
Jon Halliday <jhalliday@gmail.com>
2018-01-26 19:53:24 -0500

    Going to make a video now

---
+++ Assets/Scripts/AudioReactiveEffects/ParticleSystemEnableEmissionReactiveEffect.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.AudioReactiveEffects.Base;
+using UnityEngine;
+
+namespace Assets.Scripts.AudioReactiveEffects
+{
+    public class ParticleSystemEnableEmissionReactiveEffect : VisualizationEffectBase
+    {
+        private ParticleSystem _particleSystem;
+        private ParticleSystem.EmissionModule _emissionModule;
+
+        public override void Start()
+        {
+            base.Start();
+
+            _particleSystem = GetComponent<ParticleSystem>();
+            _emissionModule = _particleSystem.emission;
+        }
+
+        public void Update()
+        {
+            _emissionModule.enabled = GetAudioData() > PrimaryScaleFactor;
+        }
+    }
+}
